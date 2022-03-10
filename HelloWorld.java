c95ad88fecb89a44447850b191cef694e936540c
VitalyRezanov <getahur.rezanov@gmail.com>
2022-03-10 12:30:13 +0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	
+	public static void main(String[] args){
+		//System.out.println("Hello world from NC Spring School 2022");
+		for(String str : args){
+			System.out.println(str);
+		}
+	}
+	
+	
+}
